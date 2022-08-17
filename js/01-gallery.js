0dd7057e312729788dffa57f694fa1bb317e402a
Fedor <ghozztt@gmail.com>
2022-08-17 18:09:50 +0300

    make hw_07

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,57 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+// ___rendering html
+
+const galleryWrapper = document.querySelector(".gallery");
+
+const createItems = galleryItems
+  .map(
+    ({ preview, original, description }) => `<div class="gallery__item">
+<a class="gallery__link" href="${original}">
+  <img
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+  />
+</a>
+</div>`
+  )
+  .join("");
+
+galleryWrapper.insertAdjacentHTML("afterbegin", createItems);
+
+// _______getting current image
+
+galleryWrapper.addEventListener("click", clickOnWrapper);
+
+function clickOnWrapper(e) {
+  e.preventDefault();
+
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const currentUrl = e.target.dataset.source;
+  console.log(currentUrl);
+
+  //   ______using cuurentUrl in library
+
+  const modalImg = basicLightbox.create(`
+    <img src="${currentUrl}">
+`);
+  modalImg.show();
+
+  // ___add close on escape
+
+  galleryWrapper.addEventListener("keydown", clickOnEsc, { once: true });
+
+  function clickOnEsc(e) {
+    if (e.code === "Escape") {
+      modalImg.close();
+    }
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+// ___rendering html
+
+const galleryWrapper = document.querySelector(".gallery");
+
+const createItems = galleryItems
+  .map(
+    ({
+      preview,
+      original,
+      description,
+    }) => `<a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>`
+  )
+  .join("");
+
+galleryWrapper.insertAdjacentHTML("afterbegin", createItems);
+
+// ____initialization and using library
+
+var lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+  captionClass: "captions",
+});
